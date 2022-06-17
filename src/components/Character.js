2041535b536b1c642bd3c7a4c70bd0cc4b8ce69c
Matt Decker <matt@Matts-MBP.lan>
2022-06-17 14:39:42 -0400

    MVP needs style

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React, {useState} from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+
+const StyledCharacter = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    color: white;
+    background-color: black;
+    opacity: 2;
+    height: 75%;
+    margin: 2vh;
+
+    p {
+        justify-content: flex-start;
+        font-size: 3rem;
+        margin: 1vh;
+    }
+`
+
+
+export default function Character(props) {
+    return (
+        <StyledCharacter className='character-wrapper'>
+            <p>{props.name}</p>
+            <p>{props.bDay}</p>
+        </StyledCharacter>
+    )
+}
